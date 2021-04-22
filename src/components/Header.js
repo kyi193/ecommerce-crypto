244e77f6b8abba1ec9a44885e39ceba9ed8c80f1
Kevin <kevin.yi93@gmail.com>
2021-04-22 10:14:42 -0400

    Display moon and animate on page load

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
+import moon from "../img/moon-hero.png";
 
 const Header = () => {
   return (
     <header class="header">
+      <div class="header__logo-box">
+        <img src={moon} alt="Logo" class="header__logo"></img>
+      </div>
+
       <div class="header__text-box">
         <h1 class="heading-primary">
           <span class="heading-primary--main">Moon</span>
